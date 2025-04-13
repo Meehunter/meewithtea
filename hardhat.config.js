f273b58e7ca80099e1104bb7e00dcce58d983071
Meehunter <85058759+Meehunter@users.noreply.github.com>
2025-04-13 20:31:17 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
   solidity: "0.8.20",
+  paths: {
+    artifacts: "./frontend/artifacts", // biar frontend bisa akses ABI & bytecode
+  },
   networks: {
+    hardhat: {},
     tea: {
-      url: "https://tea-sepolia.g.alchemy.com/v2/j89tFiiDn7xXiIIcEofvgqS1huIVOluo", // ganti dengan RPC sebenarnya
-      chainId: 10218, // ganti dengan chain ID Tea Sepolia Testnet
+      url: "https://tea-sepolia.g.alchemy.com/v2/j89tFiiDn7xXiIIcEofvgqS1huIVOluo",
+      chainId: 10218,
     }
   }
 };
+
